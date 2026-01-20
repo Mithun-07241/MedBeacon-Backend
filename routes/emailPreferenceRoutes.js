d3661e220163adaa01f9b75c74bfbd11d2ec9282
Mithun-07241 <mithun.k245a@gmail.com>
2026-01-20 23:06:11 +0530

    fix in mail updations

--- routes/emailPreferenceRoutes.js
+++ routes/emailPreferenceRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const emailPreferenceController = require("../controllers/emailPreferenceController");
-const { protect } = require("../middleware/authMiddleware");
+const authMiddleware = require("../middleware/auth");
 
 // Public routes (token-based)
 router.get("/unsubscribe/:token", emailPreferenceController.getUnsubscribePage);
 router.post("/unsubscribe/:token", emailPreferenceController.updatePreferences);
 
 // Authenticated routes
-router.get("/my-preferences", protect, emailPreferenceController.getMyPreferences);
-router.put("/my-preferences", protect, emailPreferenceController.updateMyPreferences);
+router.get("/my-preferences", authMiddleware, emailPreferenceController.getMyPreferences);
+router.put("/my-preferences", authMiddleware, emailPreferenceController.updateMyPreferences);
 
 module.exports = router;
