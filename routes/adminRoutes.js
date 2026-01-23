12f88f6c2f92890ee10698cb27c778d4fdfcaa53
Mithun-07241 <mithun.k245a@gmail.com>
2026-01-23 20:30:29 +0530

    bug fix for admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/auth');
 const { adminOnly } = require('../middleware/adminMiddleware');
 const adminController = require('../controllers/adminController');
 
